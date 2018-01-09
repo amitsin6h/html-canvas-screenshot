f528c0bcdb89c0e3f37f0b7169d0345fead87a09
amitsin6h <amitsin6h@gmail.com>
2018-01-09 19:57:55 +0530

    updated

--- index.php
+++ index.php
@@ -7,41 +7,41 @@
 </head>
 <body>
 
-<h1 style="text-align: center;">Tutorial: How to take screenshot</h1>
-<div id="capture" style="padding: 10px; background: #f5da55">
-    <h4 style="color: #000; ">Hello C-Sharpcorner User!!</h4>
-</div>
-<div style="margin-top: 50px; text-align: center;">
-	<button id="take_screenshoot">Take Screenshot</button>
-</div>
+	<h1 style="text-align: center;">Tutorial: How to take screenshot</h1>
+	<div id="capture" style="padding: 10px; background: #f5da55">
+		<h4 style="color: #000; ">Hello C-Sharpcorner User!!</h4>
+	</div>
+	<div style="margin-top: 50px; text-align: center;">
+		<button id="take_screenshoot">Take Screenshot</button>
+	</div>
 
-<script type="text/javascript">
-	var dataURL = {};
-	$('#take_screenshoot').click(function(){
+	<script type="text/javascript">
+		var dataURL = {};
+		$('#take_screenshoot').click(function(){
 			html2canvas(document.querySelector("#capture")).then(canvas => {
-	    document.body.appendChild(canvas);
+				document.body.appendChild(canvas);
 
 	    //console.log(canvas.toDataURL());
-	   	dataURL = canvas.toDataURL();
-			post_data(dataURL);  	
+	    dataURL = canvas.toDataURL();
+	    post_data(dataURL);  	
+
+	  });
 
 		});
 
-	});
 
-
-	function post_data(imageURL){
+		function post_data(imageURL){
 		//console.log(imageURL);
 		$.ajax({
-						url: "save_data.php",
-						type: "POST",
-						data: {image: imageURL},
-						dataType: "html",
-						success: function(data) {
-							alert(data);
-							location.reload();
-						}
-					});
+			url: "save_data.php",
+			type: "POST",
+			data: {image: imageURL},
+			dataType: "html",
+			success: function() {
+				alert('Success!!');
+				location.reload();
+			}
+		});
 		
 	}
 
